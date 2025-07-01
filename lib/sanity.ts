11af8bfa3ac67a990a8d73483ed2eeef822310ed
Ramit <ramitsingh12@gmail.com>
2025-07-01 13:33:50 +0530

    projectid

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2023-10-01",
   useCdn: true,
