fbbc36df4ef20920fb3ba482dae284e45d31c433
Ramit <ramitsingh12@gmail.com>
2025-07-17 13:03:03 +0530

    changed carousel component in gallery page

--- components/sections/GallerySlider/page.tsx
+++ components/sections/GallerySlider/page.tsx
@@ -27,7 +27,7 @@ const GallerySlider: React.FC<GallerySliderProps> = ({ items }) => {
   const [index, setIndex] = useState(0);
   return (
     <div>
-      <Carousel opts={{ align: "start" }} className="w-full">
+      {/* <Carousel opts={{ align: "start" }} className="w-full">
         <CarouselContent>
           {items.map((item, index) => (
             <CarouselItem key={index} className="basis-[80%] lg:basis-1/3">
@@ -48,7 +48,26 @@ const GallerySlider: React.FC<GallerySliderProps> = ({ items }) => {
             </CarouselItem>
           ))}
         </CarouselContent>
-      </Carousel>
+      </Carousel> */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {items.map((item, i) => (
+          <div
+            key={i}
+            onClick={() => {
+              setIndex(i);
+              setOpen(true);
+            }}
+            className="relative cursor-pointer aspect-square rounded-lg overflow-hidden group"
+          >
+            <Image
+              src={item.src}
+              alt={item.description}
+              fill
+              className="object-cover transition duration-500 group-hover:scale-105 pointer-events-none"
+            />
+          </div>
+        ))}
+      </div>
       <LightBoxComponent
         open={open}
         close={() => {
